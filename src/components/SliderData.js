c579416527f153cf6132344292c1d8d06f650e44
dpsha@DESKTOP-EJNNPU <dpsharma2036@gmail.com>
2022-08-30 23:42:52 -0400

    initail commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,40 @@
+export const SliderData = [
+    {
+        image:'./images/Sunrise.jpg'
+        
+    
+    },
+    
+    {
+        image:'./images/pokhara.jpg'
+    },
+    
+    {
+        image:'./images/Everest.jpg'
+       
+    },
+    
+    {
+        image:'./images/Kalinchowk.jpg'
+    },
+
+    {
+        image:'./images/Manakamana.jpg'
+    },
+
+    {
+        image:'./images/fewa.jpg'
+    },
+
+    {
+        image:'./images/Sarankot.webp'
+    },
+
+    {
+        image:'./images/PashupatiNath.jpg'
+    },
+
+    {
+        image:'./images/sunset.jpg'
+    },
+];
